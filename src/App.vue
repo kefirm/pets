e6d814095c74be6914ca75242f200453cbd2e4af
kefirm <kefirm@tlen.pl>
2020-07-03 16:20:02 +0200

    pets vue project

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/cats">cats</router-link> |
+      <router-link to="/dogs">dogs</router-link> |
+      <router-link to="/pets">pets</router-link>
     </div>
-    <router-view/>
+    <b-container>
+      <router-view/>
+    </b-container>
   </div>
 </template>
 
---
+++ src/views/Pets.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ animal.name }} {{ $route.params.species }}</h1>
+    <p>Age: {{ animal.age }} yo</p>
+    <p> Breed: {{ animal.breed }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data() {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    /* cats() {
+        return this.$store.state.cats
+      }, */
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted() {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animal
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
